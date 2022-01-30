14970f4c604d2c0e0e26ff78b58931e40c7e7b93
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-01-30 17:03:39 +0530

    Updated Apply form link
    Form link will open in new tab rather than same page.

--- frontend/src/pages/Apply.jsx
+++ frontend/src/pages/Apply.jsx
@@ -13,6 +13,8 @@ export const Apply = () => {
       />
       <a
         href='https://forms.gle/1fFKhiPpFH452hvNA'
+        target='_blank'
+        rel='noreferrer'
         className='flex justify-center mt-4 w-1/4 m-auto'>
         <button className='bg-blue-600 text-white lg:px-9 px-4 lg:py-2 py-1 rounded-full w-full hover:shadow-lg hover:shadow-blue-300 transition-shadow duration-150 ease-in-out'>
           <span className='font-medium lg:text-lg text-sm m-auto'>

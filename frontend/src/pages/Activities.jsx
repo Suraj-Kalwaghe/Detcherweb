762312053799fce53f73a2da16bab90c80082491
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-01-30 17:00:14 +0530

    Refactored Activities.jsx

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -1,67 +1,29 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+
+import { Container } from '../components';
 
 export const Activities = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [formMessage, setFormMessage] = useState('');
-
-  async function handleFormSubmit(data) {
-    const response = await axios.post('/api/v1', data);
-    if (response.statusText === 'OK')
-      setFormMessage('Form Submitted Successfully');
-    else setFormMessage(`Error Occured ${response.status}`);
-  }
-
-  const handleForm = e => {
-    e.preventDefault();
-    handleFormSubmit({ name: name, email: email, message: message });
-    setName('');
-    setEmail('');
-    setMessage('');
-    setTimeout(() => {
-      setFormMessage('');
-    }, 10000);
-  };
-
-  const customFilter = {
-    filter: 'grayscale(100%) contrast(120%) opacity(16%)',
-  };
-
   return (
-    <div>
-      <table
-        // style='margin:0 0 10px 0; width:244px; background:#fff; border:1px solid #ccc;'
-        cellspacing='0'
-        cellpadding='0'>
-        <tr>
-          <td>
-            <a href='https://www.miniclip.com/games/short-ride/en/'>
-              <img
-                src='https://static.miniclipcdn.com/content/game-icons/small/shortride_70x59.jpg'
-                width='70'
-                height='59'
-                align='left'
-                alt='Games at Miniclip.com - Short Ride'
-              />
-              <a href='https://www.miniclip.com/games/short-ride/en/'>
-                Short Ride
-              </a>
-              <p>It's a Short Life! With so many dumb ways to die!</p>
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
+    <Container>
+      <div className='flex flex-wrap'>
+        <div className='border border-slate-300 rounded-lg flex xl:w-1/4 md:w-1/2 w-full h-1/3'>
+          <img
+            src='https://static.miniclipcdn.com/content/game-icons/small/shortride_70x59.jpg'
+            alt='Games at Miniclip.com - Short Ride'
+            className='w-2/5 rounded-l-lg'
+          />
+          <div className='my-2 mx-2'>
             <a
               href='https://www.miniclip.com/games/short-ride/en/'
-              title='Games at Miniclip.com'>
-              Play this free game now!
+              className='text-blue-700 underline font-bold text-lg'>
+              Short Ride
             </a>
-          </td>
-        </tr>
-      </table>
-    </div>
+            <p className='font-medium text-slate-500 mt-1.5'>
+              It's a Short Life! With so many dumb ways to die!
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 };

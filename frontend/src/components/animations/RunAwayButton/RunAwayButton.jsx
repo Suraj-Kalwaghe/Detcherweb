22be3df08e6a642cadb2d93010773a061d7066fa
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-02-01 22:50:45 +0530

    Added: RunAwayButton

---
+++ frontend/src/components/animations/RunAwayButton/RunAwayButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import anime from 'animejs';
+
+import './styles.css';
+
+export const RunAwayButton = ({ text }) => {
+  React.useEffect(() => {
+    const button = document.getElementById('runaway-btn');
+    // setMainButton(button);
+    const animateMove = (element, prop, pixels) => {
+      anime({
+        targets: element,
+        [prop]: `${pixels}px`,
+        easing: 'easeOutCirc',
+      });
+    };
+    const actions = ['mouseover', 'click'];
+    actions.forEach(function (el) {
+      button.addEventListener(el, function (event) {
+        const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+        const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+        animateMove(this, 'left', left).play();
+        animateMove(this, 'top', top).play();
+      });
+    });
+
+    const getRandomNumber = num => {
+      return Math.floor(Math.random() * (num + 1));
+    };
+  });
+
+  return (
+    <>
+      <button id='runaway-btn' className='runAwayButton'>
+        <span className='text-xl font-bold text-slate-500 border border-slate-300 w-1/5 rounded-2xl px-20 py-7 shadow-md'>
+          {text}
+        </span>
+      </button>
+    </>
+  );
+};

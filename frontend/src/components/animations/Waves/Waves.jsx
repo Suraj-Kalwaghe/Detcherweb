591c3ed2316b5f66e0fc9456d08901c71c5d2293
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-02-01 22:51:27 +0530

    Update: Z-index

--- frontend/src/components/animations/Waves/Waves.jsx
+++ frontend/src/components/animations/Waves/Waves.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 
 export const Waves = () => {
   return (
-    <div className='ocean'>
+    <div className='ocean z-10'>
       <div className='wave'></div>
       <div className='wave'></div>
     </div>

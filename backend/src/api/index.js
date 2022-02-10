353a451d309767c14ce41f78979c6578bee1c8a2
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-02-10 22:26:04 +0530

    Update: MongoDB to MySQL

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,22 +1,40 @@
 const express = require('express');
-const monk = require('monk');
 const Joi = require('joi');
+const mysql = require('mysql2');
 
-const emojis = require('./emojis');
 const router = express.Router();
 
-const db = monk(process.env.MONGO_URI);
-const forms = db.get('forms');
+// Main connection to our cloud DB instance.
+// Always use .env files to store sensitive information.
+const connection = mysql.createConnection({
+  host: process.env.DB_TEST_HOST,
+  user: process.env.DB_TEST_USERNAME,
+  password: process.env.DB_TEST_PASSWORD,
+  database: process.env.DB_TEST_NAME,
+});
+
+// Validation of the recieved data from client is handled here.
 const schema = Joi.object({
   name: Joi.string().trim().required(),
   email: Joi.string().trim().required(),
   message: Joi.string().trim().required(),
 });
 
+// Creating table if doesn't exist in cloud server DB.
+connection.execute(
+  'CREATE TABLE IF NOT EXISTS `test` (name VARCHAR(100), email VARCHAR(200), message VARCHAR(300))',
+  (err, items) => {
+    if (err) throw err;
+    console.log('Created Table');
+  }
+);
+
 router.get('/', async (req, res, next) => {
   try {
-    const items = await forms.find({});
-    res.json(items);
+    connection.execute('SELECT * FROM test', (err, items) => {
+      if (err) throw err;
+      res.json(items);
+    });
   } catch (error) {
     next(error);
   }
@@ -25,13 +43,17 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const value = await schema.validateAsync(req.body);
-    const inserted = await forms.insert(value);
-    res.json(inserted);
+    connection.execute(
+      'INSERT INTO test (name, email, message) VALUES (?,?,?)',
+      [value.name, value.email, value.message],
+      (err, items) => {
+        if (err) next(err);
+        res.json(items);
+      }
+    );
   } catch (error) {
     next(error);
   }
 });
-
-router.use('/emojis', emojis);
 
 module.exports = router;
